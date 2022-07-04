a94e3c72c632bdb68df7af5cb89f82fbbff20cee
juliogayosso <j_chalo@hotmail.com>
2022-07-04 14:46:04 -0500

    entregable2

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import AppiWeather from './components/AppiWeather'
+function App() {
+  return (
+    <div className="App">
+      <AppiWeather />
+    </div>
+  )
+}
+export default App
